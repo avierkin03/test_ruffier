29a355a29682bc7144a709cc9693cf3e73219df1
avierkin03 <93377331+avierkin03@users.noreply.github.com>
2024-01-19 23:09:22 +0200

    Add files via upload

---
+++ my_app.py
@@ -0,0 +1,44 @@
+# напиши тут код основної програми та першого екрану
+from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
+from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont # перевірка типів значень, що вводяться
+from PyQt5.QtWidgets import*
+
+from instructions import *
+from second_win import *
+
+#вікно, в якому розташовується привітання  
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.connects()
+        self.set_appear()
+        self.show()
+
+    #створює графічні елементи
+    def initUI(self):
+        self.btn_next = QPushButton(txt_next, self)
+        self.hello_text = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+        self.layout_line = QVBoxLayout()
+        self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignLeft)
+        self.layout_line.addWidget(self.instruction, alignment = Qt.AlignLeft) 
+        self.layout_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)          
+        self.setLayout(self.layout_line)
+
+    def next_click(self):
+        self.tw = TestWin()
+        self.hide()
+
+    def connects(self):
+        self.btn_next.clicked.connect(self.next_click)
+
+    #встановлює, як виглядатиме вікно (напис, розмір, місце)
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+
+app = QApplication([])
+mw = MainWin()
+app.exec_()
