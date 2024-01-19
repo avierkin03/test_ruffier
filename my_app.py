8612dde2d1e2253a3fa22db682c37dd5f75980e2
avierkin03 <93377331+avierkin03@users.noreply.github.com>
2024-01-19 23:10:48 +0200

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,4 +1,3 @@
-# напиши тут код основної програми та першого екрану
 from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
 from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont # перевірка типів значень, що вводяться
 from PyQt5.QtWidgets import*
